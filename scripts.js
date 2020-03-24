3b3cb9b9a73ee72060805bb9aa4a9356fa3e816e
KillianDEUX <killiandeux@gmail.com>
2020-03-24 15:45:49 +0000

    Amélioration des résultats complémentaires, touche entrée pour rechercher, image de base quand poster non disponibles, acteurs cliquables

--- scripts.js
+++ scripts.js
@@ -1,4 +1,12 @@
-// Affiche de toutes les données saisies ou choisies
+
+function doFormStuff() {
+    return false;
+}
+
+function faire_alert(text){
+  alert(text);
+}
+
 
 function chargement() {
 
@@ -14,7 +22,7 @@ function chargement() {
 
 
 		  if (request.status >= 200 && request.status < 400) {
-
+             // TITRE CHARGE
         const premier_film = data.results[0];
         const h1 = document.getElementById('titreFilmOriginel');
         h1.innerHTML = "";
@@ -26,36 +34,37 @@ function chargement() {
         balise_titre.innerHTML = "";
         balise_titre.textContent = premier_film.title;
 
-         const balise_resume = document.getElementById('resumeFilm')
-         balise_resume.innerHTML = "";
-         balise_resume.textContent = premier_film.overview;         
+        const balise_resume = document.getElementById('resumeFilm')
+        balise_resume.innerHTML = "";
+        balise_resume.textContent = premier_film.overview;         
 
-         data.results.shift();
-         const boutons_propositions = document.getElementById('boutons_propositions');
-         boutons_propositions.innerHTML = "";
-			   data.results.forEach(movie => {
-				       const h1 = document.createElement('button');
-			 	       h1.textContent =  movie.title;
-               var titre_du_film = movie.title;
+        data.results.shift();
+        
+			  var j, html;
+        html = "Voici une liste de résultats supplémentaires avec votre recherche :";
+        html += "<form id=\"listes\" name=\"listes\">";
+        html += "<span id=\"numero\"></span> <select name=\"liste\" onChange = chargement_lien(this.value) ><option value=\"\">Choisir...</option>";
+        for (j=0; j<data.results.length; j++){
+            html += "<option value=\""+data.results[j].title+"\" onSelect= faire_alert(\"" + "couc" + "\")>"+data.results[j].title+"</option>";
+        }
+        html += "</select><br>";
+        html +="</form>";
 
-               h1.addEventListener("click", function () {
-                 chargement_lien(titre_du_film);
-               });
-				      boutons_propositions.appendChild(h1);
-			  })
+        const lstderoule = document.getElementById('listederoulante');
+        lstderoule.innerHTML = html;
 
-      //traitement de l'image
-      image = premier_film.backdrop_path;
-      balise_image = document.getElementById('Affichage_FanArt');
-      balise_image.setAttribute('style', "background-image: url('https://image.tmdb.org/t/p/original"+ image+"')" );
+        //traitement de l'image
+        image = premier_film.backdrop_path;
+        balise_image = document.getElementById('Affichage_FanArt');
+        balise_image.setAttribute('style', "background-image: url('https://image.tmdb.org/t/p/original"+ image+"')" );
 
-      charger_acteur(premier_film);
+        charger_acteur(premier_film);
 
-		} else {
-			const errorMessage = document.createElement('marquee');
-			errorMessage.textContent = 'Erreur URL API';
-			app.appendChild(errorMessage);
-		}
+      } else {
+        const errorMessage = document.createElement('marquee');
+        errorMessage.textContent = 'Erreur URL API';
+        app.appendChild(errorMessage);
+      }
 		//alert("fin traitement");
 	}
 	request.send()
@@ -70,43 +79,73 @@ function charger_acteur(premier_film){
 	    request_acteur.onload = function(){
 	 	    var data = JSON.parse(this.response);
         
-         if (request_acteur.status >= 200 && request_acteur.status < 400) {
-
-            premier_acteur = data.cast[0];
-            const img_premier_acteur = document.getElementById('img_premier_acteur');
-            img_premier_acteur.setAttribute('src', "https://image.tmdb.org/t/p/original"+ premier_acteur.profile_path);
+        if (request_acteur.status >= 200 && request_acteur.status < 400) {
+          
+          premier_acteur = data.cast[0];
+          const img_premier_acteur = document.getElementById('img_premier_acteur');
+          img_premier_acteur.setAttribute('src', "https://image.tmdb.org/t/p/original"+ premier_acteur.profile_path);
             
-            const nom_premier_acteur = document.getElementById('nom_premier_acteur');
-            nom_premier_acteur.innerHTML = "";
-            nom_premier_acteur.textContent = premier_acteur.name;
+          const nom_premier_acteur = document.getElementById('nom_premier_acteur');
+          nom_premier_acteur.innerHTML = "";
+          nom_premier_acteur.textContent = premier_acteur.name;
+          nom_premier_acteur.setAttribute("href", "http://www.google.com/search?q="+premier_acteur.name);
+          
 
-            const role_premier_acteur = document.getElementById('role_premier_acteur');
-            role_premier_acteur.innerHTML = "";
-            role_premier_acteur.textContent = premier_acteur.character;
-
+          const role_premier_acteur = document.getElementById('role_premier_acteur');
+          role_premier_acteur.innerHTML = "";
+          role_premier_acteur.textContent = premier_acteur.character;
+          
+          if(data.cast.length > 1){
             deuxieme_acteur = data.cast[1];
             const img_deuxieme_acteur = document.getElementById('img_deuxieme_acteur');
             img_deuxieme_acteur.setAttribute('src', "https://image.tmdb.org/t/p/original"+ deuxieme_acteur.profile_path);
-            
+              
             const nom_deuxieme_acteur = document.getElementById('nom_deuxieme_acteur');
             nom_deuxieme_acteur.innerHTML = "";
             nom_deuxieme_acteur.textContent = deuxieme_acteur.name;
+            nom_deuxieme_acteur.setAttribute("href", "http://www.google.com/search?q="+deuxieme_acteur.name)
 
             const role_deuxieme_acteur = document.getElementById('role_deuxieme_acteur');
             role_deuxieme_acteur.innerHTML = "";
             role_deuxieme_acteur.textContent = deuxieme_acteur.character;
+            if(data.cast.length > 2){
+              troisieme_acteur = data.cast[2];
+              const img_troisieme_acteur = document.getElementById('img_troisieme_acteur');
+              img_troisieme_acteur.setAttribute('src', "https://image.tmdb.org/t/p/original"+ troisieme_acteur.profile_path);
+                
+              const nom_troisieme_acteur = document.getElementById('nom_troisieme_acteur');
+              nom_troisieme_acteur.innerHTML = "";
+              nom_troisieme_acteur.textContent = troisieme_acteur.name;
+              nom_troisieme_acteur.setAttribute("href", "http://www.google.com/search?q="+troisieme_acteur.name)
 
-            troisieme_acteur = data.cast[2];
+              const role_troisieme_acteur = document.getElementById('role_troisieme_acteur');
+              role_troisieme_acteur.innerHTML = "";
+              role_troisieme_acteur.textContent = troisieme_acteur.character;
+            }
+            else{
+              const img_troisieme_acteur = document.getElementById('img_troisieme_acteur');
+              img_troisieme_acteur.style.display='none';
+              const nom_troisieme_acteur = document.getElementById('nom_troisieme_acteur');
+              nom_troisieme_acteur.innerHTML = "";
+              nom_troisieme_acteur.setAttribute("href", "http://www.google.com/search?q="+troisieme_acteur.name)
+              const role_troisieme_acteur = document.getElementById('role_troisieme_acteur');
+              role_troisieme_acteur.innerHTML = "";
+            }
+          }
+          else{
+            const img_deuxieme_acteur = document.getElementById('img_deuxieme_acteur');
+            img_deuxieme_acteur.style.display='none';
+            const nom_deuxieme_acteur = document.getElementById('nom_deuxieme_acteur');
+            nom_deuxieme_acteur.innerHTML = "";
+            const role_deuxieme_acteur = document.getElementById('role_deuxieme_acteur');
+            role_deuxieme_acteur.innerHTML = "";
             const img_troisieme_acteur = document.getElementById('img_troisieme_acteur');
-            img_troisieme_acteur.setAttribute('src', "https://image.tmdb.org/t/p/original"+ troisieme_acteur.profile_path);
-            
+            img_troisieme_acteur.style.display='none';
             const nom_troisieme_acteur = document.getElementById('nom_troisieme_acteur');
             nom_troisieme_acteur.innerHTML = "";
-            nom_troisieme_acteur.textContent = troisieme_acteur.name;
-
             const role_troisieme_acteur = document.getElementById('role_troisieme_acteur');
             role_troisieme_acteur.innerHTML = "";
-            role_troisieme_acteur.textContent = troisieme_acteur.character;
+          }
         } 
         else{
           const errorMessage = document.createElement('marquee');
@@ -119,7 +158,6 @@ function charger_acteur(premier_film){
 
 
 function chargement_lien(titre) {
-
 	  const app = document.getElementById('root');
   	const films_disponibles = document.getElementById('films_disponibles');
 
@@ -146,22 +184,31 @@ function chargement_lien(titre) {
           balise_resume.textContent = premier_film.overview;
 
           data.results.shift();
-          data.results.forEach(movie => {
-                  const h1 = document.createElement('button');
-                  h1.textContent =  movie.title;
-                  var titre_du_film = movie.title;
+          var j, html;
+          html = "Voici une liste de résultats avec votre recherche :";
+          html += "<form id=\"listes\" name=\"listes\">";
+          html += "<span id=\"numero\"></span> <select name=\"liste\"><option value=\"\">Choisir...</option>";
+          for (j=0; j<data.results.length; j++){
+            html += "<option value=\""+data.results[j].title+"\" onClick= chargement_lien(value)>"+data.results[j].title+"</option>"; // chargement_lien(\""+data.results[j].title+"\")
+          }
+          html += "</select><br>";
+          html +="</form>";
 
-                  h1.addEventListener("click", function () {
-                    chargement_lien(titre_du_film);
-                  });
-                  boutons_propositions.appendChild(h1);
-          })
+          const lstderoule = document.getElementById('listederoulante');
+          lstderoule.innerHTML = html;
 
           //traitement de l'image
           image = premier_film.backdrop_path;
-          balise_image = document.getElementById('Affichage_FanArt');
-          balise_image.setAttribute('style', "background-image: url('https://image.tmdb.org/t/p/original"+ image+"')" );
-          charger_acteur(premier_film);
+          if (image==null){
+            balise_image = document.getElementById('Affichage_FanArt');
+            balise_image.setAttribute('style', "background-image: url('img/bg-masthead.gif')" );
+            charger_acteur(premier_film);
+          }else{
+            balise_image = document.getElementById('Affichage_FanArt');
+            balise_image.setAttribute('style', "background-image: url('https://image.tmdb.org/t/p/original"+ image+"')" );
+            charger_acteur(premier_film);
+          }
+
         } 
         
         else{
